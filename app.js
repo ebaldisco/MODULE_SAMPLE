ef94cfd07d03e7554fe5813398081110836bf6d1
Emmanuel <ebaldisco@gmail.com>
2016-07-14 19:04:51 +0800

    Module_Sample 1

---
+++ app.js
@@ -0,0 +1,14 @@
+var line_Space = '\n';
+
+const util = require('./my_module');
+
+var area = util.computeArea(10,50);
+
+console.log(line_Space);
+
+console.log("\tArea is : " +area);
+
+var perimeter = util.computePerimeter(10,50);
+
+console.log("\tPerimeter is : " +perimeter);
+console.log(line_Space);
